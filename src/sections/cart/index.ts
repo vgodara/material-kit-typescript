98c01ec11549e29fbd588afb9fcb7a09f5d393e1
Vikas Godara <v.godara@live.com>
2023-04-06 10:30:53 +0530

    Fixed name of delivery option from delivery address

--- src/sections/cart/index.ts
+++ src/sections/cart/index.ts
@@ -1,6 +1,6 @@
 export {default as AddressSection} from './Address'
 export {default as BillingAddressSection} from './BillingAddress'
-export {default as DeliveryAddressSection} from './DeliveryOptions'
+export {default as DeliveryOptionSection} from './DeliveryOptions'
 export {default as OrderSummarySection} from './OrderSummary'
 export {default as PaymentOptionsSection} from './PaymentOptions'
 export {default as ItemsToBuySection} from './ItemsToBuy'
