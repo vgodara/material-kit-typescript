3f979af5cd35f4d4297dd302b59b981dec6eb6bb
Vikas Godara <v.godara@live.com>
2023-04-06 09:43:31 +0530

    Added helper text to Counter component

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,12 +1,26 @@
 import CounterProps from "./types";
-import {IconButton, InputAdornment, OutlinedInput} from "@mui/material";
+import {IconButton, InputAdornment, styled, TextField} from "@mui/material";
 import Iconify from "../iconify";
 import {useState} from "react";
 
+const StyledTextField = styled(TextField)(({theme}) => ({
+    '& .MuiInputBase-root': {
+        padding: theme.spacing(0),
+    },
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(0.75,0)
+
+    },
+    '& .MuiFormHelperText-root': {
+        textAlign: 'right',
+       margin:theme.spacing(1,0,0,0)
+    }
+}))
 export default function Counter({
                                     initial = 0,
                                     min = Number.MIN_SAFE_INTEGER,
                                     max = Number.MAX_SAFE_INTEGER,
+                                    helperText,
                                     setQuantity
                                 }: CounterProps) {
     const [value, setValue] = useState(initial)
@@ -21,25 +35,26 @@ export default function Counter({
         return updatedValue
     })
 
-    return <OutlinedInput type={'button'}
-                          size={'small'}
-                          sx={(theme) => ({
-                              px: theme.spacing(0.6)
-                          })}
-                          value={value}
-                          startAdornment={<InputAdornment
+    return <StyledTextField type={'button'}
+                      size={'small'}
+                      helperText={helperText}
+                      value={value}
+                      InputProps={{
+                          startAdornment: (<InputAdornment
                               position={'start'}
                               component={IconButton}
                               onClick={decrementValue}
                               disabled={value <= min}
                           ><Iconify width={16} icon={'eva:minus-fill'}/>
-                          </InputAdornment>}
-                          endAdornment={<InputAdornment
+                          </InputAdornment>),
+                          endAdornment: (<InputAdornment
                               position={'end'}
                               component={IconButton}
                               onClick={incrementValue} disabled={value >= max}
                           ><Iconify width={16} icon={'eva:plus-fill'}/>
-                          </InputAdornment>}
+                          </InputAdornment>)
+                      }}
+
     />
 
 }
--- src/components/counter/types.ts
+++ src/components/counter/types.ts
@@ -1,6 +1,7 @@
- export default interface CounterProps {
-    initial?:number
-     min?:number
-     max?:number
-     setQuantity :(value:number)=>void
- }
+export default interface CounterProps {
+    initial?: number
+    min?: number
+    max?: number
+    helperText?: string
+    setQuantity: (value: number) => void
+}
