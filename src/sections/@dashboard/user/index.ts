fd64f37c1f13a9351a034bf78763dccacb4e38d0
Vikas Godara <v.godara@live.com>
2023-04-29 14:22:03 +0530

    Added SocialMediaLink Section

--- src/sections/@dashboard/user/index.ts
+++ src/sections/@dashboard/user/index.ts
@@ -2,3 +2,4 @@ export { default as UserListHead } from './UserListHead';
 export { default as UserListToolbar } from './UserListToolbar';
 export { default as UserCardSection } from './UserCard';
 export {default as UserOverViewSection} from './UserOverView'
+export {default as SocialMediaLinkSection} from './SocialMediaLink'
