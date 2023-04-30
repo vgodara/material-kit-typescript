f8bf838d6a3a35f4bbeda300bab90e66d1ff87bf
Vikas Godara <v.godara@live.com>
2023-04-30 08:52:07 +0530

    Add section which allows user to post text and/or media

--- src/sections/@dashboard/user/index.ts
+++ src/sections/@dashboard/user/index.ts
@@ -2,3 +2,5 @@ export { default as UserListHead } from './UserListHead';
 export { default as UserListToolbar } from './UserListToolbar';
 export { default as UserCardSection } from './UserCard';
 export {default as UserOverViewSection} from './UserOverView'
+export {default as SocialMediaLinkSection} from './SocialMediaLink'
+export {default as UserCreatePostSection} from './UserCreatePost'
