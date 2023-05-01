f3f7d4a7db3d22b3a7976239f802771d43c56d84
Vikas Godara <v.godara@live.com>
2023-05-01 17:49:36 +0530

    4 Add section which show post with  likes and comments

--- src/sections/@dashboard/user/types.ts
+++ src/sections/@dashboard/user/types.ts
@@ -7,6 +7,11 @@ export interface User {
     id: string,
     status: string
 }
+export interface BasicUserInfo {
+    id:string,
+    name:string,
+    avatar:string
+}
 
 interface UserCard {
     name: string,
@@ -36,6 +41,32 @@ interface SocialMediaLinks {
 
 
 
+
+export interface Interaction  {
+    type:'like'|'comment'
+    date:Date,
+    userInfo:BasicUserInfo
+}
+ interface Like extends Interaction{
+    type:'like'
+}
+export const isLike = (interaction: Interaction): interaction is Like => interaction.type === "like";
+
+export interface Comment extends Interaction {
+    type:'comment'
+    comment:string
+
+}
+export const isComment = (interaction: Interaction): interaction is Comment => interaction.type === "comment";
+
+interface Post {
+    media:string
+    content:string,
+    user:BasicUserInfo,
+    date:Date
+    interactions:Interaction[]
+}
+
 export interface UserTableToolbarProps {
     numSelected: number,
     filterName: string,
@@ -53,4 +84,8 @@ export interface UserOverViewProps {
 
 export interface SocialMediaLinkProps {
     links: SocialMediaLinks
+}
+
+export interface UserPostProps {
+    post:Post
 }
