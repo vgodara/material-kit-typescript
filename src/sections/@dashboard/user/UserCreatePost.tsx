b7ddae1596dc27b456901b7c7ff9467eefba24a5
vgodara <viki.godara@gmail.com>
2023-05-08 16:25:30 +0530

    Add section which allows user to post text and/or media (#25)

---
+++ src/sections/@dashboard/user/UserCreatePost.tsx
@@ -0,0 +1,58 @@
+import {Box, Button, Card, CardContent, Fab, FormControl, Stack, TextField} from "@mui/material";
+import React, {useState} from "react";
+import {nonNullOnly} from "../../../utils/helper";
+import {useDropzone} from "react-dropzone";
+import Iconify from "../../../components/iconify";
+
+export default function UserCreatePost() {
+    const [postText, setPostText] = useState<string>('')
+    const [selectedImage,setSelectedImag]=useState('')
+    return (<>
+        <Card>
+            <CardContent>
+                <FormControl component={TextField}
+                             variant={'outlined'}
+                             fullWidth
+                             placeholder={'Share what you are thinking'}
+                             value={postText}
+                             multiline
+                             rows={3}
+                />
+                <Stack mt={3} direction={'row'} justifyContent={'space-between'}>
+                    <AddImage selectedImage={setSelectedImag}/>
+                    <Button variant={'contained'}>Post</Button>
+                </Stack>
+            </CardContent>
+        </Card>
+    </>)
+}
+
+export function AddImage({selectedImage}: {
+    selectedImage: (image: string) => void
+}) {
+    const [image, setImage] = useState<string | undefined>()
+    const onDrop = (acceptedFiles: File[]) => {
+        const fileAsUrl = acceptedFiles.filter((_, index) => index === 0).map((file) => URL.createObjectURL(file)).find(() => true)
+        setImage(fileAsUrl)
+        nonNullOnly(image, selectedImage)
+    }
+
+    const {
+        getRootProps,
+        getInputProps
+    } = useDropzone({
+        multiple: false,
+        onDrop,
+        accept: {
+            'image/*': []
+        }
+    });
+
+    return (<Box {...getRootProps()}>
+        <input {...getInputProps()} />
+        <Fab variant={'extended'} size={'small'}>
+
+            <Iconify sx={{mr: 1}} color={'primary'} icon={'ic:round-perm-media'}/>
+            Image/Video</Fab>
+    </Box>)
+}
--- src/sections/@dashboard/user/index.ts
+++ src/sections/@dashboard/user/index.ts
@@ -4,3 +4,4 @@ export { default as UserCardSection } from './UserCard';
 export {default as UserOverViewSection} from './UserOverView'
 export {default as SocialMediaLinkSection} from './SocialMediaLink'
 export {default as UserPostSection} from './UserPost'
+export { default as UserCreatePostSection } from './UserCreatePost'
