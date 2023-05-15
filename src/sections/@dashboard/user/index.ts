fc2fcc131d9cdfbe1d95d010c71ecff3377595c0
Vikas Godara <v.godara@live.com>
2023-05-15 17:48:42 +0530

    Removing unnecessary section form index

--- src/sections/@dashboard/user/index.ts
+++ src/sections/@dashboard/user/index.ts
@@ -1,8 +1,4 @@
 export { default as UserListHead } from './UserListHead';
 export { default as UserListToolbar } from './UserListToolbar';
 export { default as UserCardSection } from './UserCard';
-export {default as UserOverViewSection} from './profile/UserOverView'
-export {default as SocialMediaLinkSection} from './profile/SocialMediaLink'
-export {default as UserPostSection} from './profile/UserPost'
-export { default as UserCreatePostSection } from './profile/UserCreatePost'
-export { default as UserPostOverviewSection } from './profile/UserPostOverView';
+
