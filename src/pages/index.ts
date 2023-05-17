042b7fdea7b4ee339505f3b8d35053c6c35a9e3c
vgodara <viki.godara@gmail.com>
2023-05-17 08:32:36 +0530

    Added UserProfile Page (#38)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
-
 export {default as Page404} from './Page404'
-export {UserPage,LoginPage,UserCreatePage} from './users'
+export {UserPage,LoginPage,UserCreatePage,UserProfilePage} from './users'
 export {default as DashboardAppPage} from './DashboardAppPage'
 export {default as MailPage} from './MailPage'
 export {ProductCreatePage,ProductListPage,ProductsListingPage,ProductDetailPage,CheckoutPage} from './products'
