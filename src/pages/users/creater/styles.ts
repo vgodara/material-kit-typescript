80c0882b9d5d05b006eab5a7cefec74c4001cd35
vgodara <viki.godara@gmail.com>
2023-05-24 16:20:28 +0530

    Added User Account Page (#62)

---
+++ src/pages/users/creater/styles.ts
@@ -0,0 +1,9 @@
+import {styled} from "@mui/material";
+import Tab from "@mui/material/Tab";
+
+export const StyledTab = styled(Tab)(({theme}) => ({
+    margin: 0,
+    padding: 0,
+    minHeight: 48,
+    minWidth: 48,
+}))
